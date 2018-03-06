091f802d7507b9ac8b7f8c3629a1fc63b047148b
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-06 09:24:09 -0300

    teste server

---
+++ tddcookbook/tests_functional/test_server.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+print(browser.name)
+print(browser.current_url)
+
+assert 'Rapadura' in browser.title
