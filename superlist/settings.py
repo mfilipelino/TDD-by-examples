5c6f1502227390226710ed770150ca7f7735f737
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-07 08:22:24 -0300

    rename tddbookbook superlist

--- superlist/settings.py
+++ superlist/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tddcookbook.urls'
+ROOT_URLCONF = 'superlist.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tddcookbook.wsgi.application'
+WSGI_APPLICATION = 'superlist.wsgi.application'
 
 
 # Database
