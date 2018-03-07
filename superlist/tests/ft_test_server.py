200761b6dfa6a126b9e47c65e9c43d8aaba6985f
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-07 11:43:44 -0300

    add app list

--- superlist/tests/ft_test_server.py
+++ superlist/tests/ft_test_server.py
@@ -3,15 +3,17 @@ from unittest import TestCase
 from selenium import webdriver
 
 
+# User story: Acessando o site
 class ServerSideTest(TestCase):
-
     def setUp(self):
+        # step: 1 open the browser
         self.browser = webdriver.Chrome()
 
-    # servidor esta de rodando
     def test_run_server(self):
+        # step 2 open the website
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title, 'Servidor não está rodando')
+        # step 3 validate title page
+        self.assertIn('To-Do', self.browser.title, 'Servidor não está rodando')
 
     def tearDown(self):
         self.browser.quit()
