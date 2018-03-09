d00b9d7fe5207e0224a040a9c31712f11fceec5c
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-09 08:57:26 -0300

    especificando os testes funcionais

--- superlist/tests.py
+++ superlist/tests.py
@@ -1,5 +1,8 @@
+import time
+
 from django.test import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class HomePageTestCase(TestCase):
@@ -7,9 +10,31 @@ class HomePageTestCase(TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
-    def test_home(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do' in self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peackong feathers' for row in rows)
+        )
+
+        self.fail('Finish the test')
+
 
     def tearDown(self):
         self.browser.quit()
