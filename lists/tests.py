11c90d98633ab4c24c3d3fa0c89298daf5abebbc
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-09 09:29:40 -0300

    testes

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
 
 from lists.views import home_page
 
@@ -13,9 +12,4 @@ class ListTestCase(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        html = response.content.decode('utf-8')
-
-        self.assertTrue(html.startswith('<!DOCTYPE html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
