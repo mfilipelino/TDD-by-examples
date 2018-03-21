81d646d3abdfc48c4fbe58125b630a4a82e1cc32
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-21 10:22:47 -0300

    add model Item

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
+from lists.models import Item
 
 
 class ListTestCase(TestCase):
@@ -19,5 +20,18 @@ class ListTestCase(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_one(self):
-        pass
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'Item 1 da lista'
+        first_item.save()
+        item_id = first_item.id
+        item = Item.objects.get(id=item_id)
+        self.assertIn(first_item.text, item.text)
+
+    def test_list_items(self):
+        Item(text='item1').save()
+        Item(text='item2').save()
+        self.assertEqual(Item.objects.count(), 2)
