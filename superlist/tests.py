51b3ff90775ccb2adc02b691326cc4fa47877fc0
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-21 09:39:21 -0300

    removendo duplicação nos testes funcionais

--- superlist/tests.py
+++ superlist/tests.py
@@ -10,6 +10,11 @@ class HomePageTestCase(TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -27,14 +32,14 @@ class HomePageTestCase(TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # inputbox.send_keys('Use peacock feathers to make a fly')
-        # inputbox.send_keys(Keys.ENTER)
-        # time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('td')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        # self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('Buy peacock feathers')
         self.fail('Finish the test')
 
 
