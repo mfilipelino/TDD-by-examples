00856cdf5ca9c7093aa1c057fe6c45a58103d60f
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-22 13:30:08 -0300

    remove sleeps voodus

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,22 +1,33 @@
 import time
 
-from django.test import TestCase
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
-class HomePageTestCase(TestCase):
-
+class HomePageTestCase(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -30,18 +41,14 @@ class HomePageTestCase(TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
-        self.check_for_row_in_list_table('Buy peacock feathers')
+        self.wait_for_row_in_list_table('Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('Buy peacock feathers')
         self.fail('Finish the test')
-
 
     def tearDown(self):
         self.browser.quit()
