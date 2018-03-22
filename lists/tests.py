0887e73da8b9c880073c1882535e1e2aecab135f
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-22 08:55:09 -0300

    add mais um test de redirect

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,6 @@ from lists.models import Item
 
 
 class ListTestCase(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -17,20 +16,22 @@ class ListTestCase(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-        self.assertIn('A new list item', response.context.get('new_item_text'))
-        self.assertTemplateUsed(response, 'home.html')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.get('location'), '/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
 
-
 class ItemModelTest(TestCase):
-
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'Item 1 da lista'
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 
@@ -6,8 +6,6 @@ def home_page(request):
 
     if request.method == 'POST':
         item = Item.objects.create(text=request.POST.get('item_text', ''))
-        new_item_text = item.text
-    else:
-        new_item_text = ''
+        return redirect('/')
 
-    return render(request, 'home.html', dict(new_item_text=new_item_text))
+    return render(request, 'home.html')
