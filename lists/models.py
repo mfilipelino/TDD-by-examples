2eb8ea1d81f6cb6d997ae06069560797d31c0766
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-28 09:29:59 -0300

    add List model

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Item(models.Model):
-
     text = models.CharField(max_length=50, default='')
+    list = models.ForeignKey('List', default=None, blank=True)
+
+
+class List(models.Model):
+    pass
--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class ListTestCase(TestCase):
@@ -13,17 +13,6 @@ class ListTestCase(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.get('location'), '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -58,4 +47,41 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+
+class ListAndItemModels(TestCase):
+
+    def test_create_list(self):
+        list_ = List()
+        list_.save()
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        first_item = Item.objects.create(text='The first (ever) list item')
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item.objects.create(text='Item the second')
+        second_item.list = list_
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(Item.objects.filter(list=list_).count(), 2)
+
+
+
 
